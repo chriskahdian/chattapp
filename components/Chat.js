d55baf65102d44749d0523d1d17e4f934d5874b7
Chris Kahdian <chriskahdian@gmail.com>
2021-03-03 10:46:52 -0800

    202103031046.05.03

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,96 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, Platform, KeyboardAvoidingView} from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
+  componentDidMount() {
     let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
-    let color = this.props.route.params.color;
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: name+' has entered this chat',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  renderBubble(props) {
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: color}}>
-        <Text>Hello Screen2!</Text>
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
+  }
+
+  render() {
+        let name = this.props.route.params.name;
+        this.props.navigation.setOptions({ title: name });
+        let color = this.props.route.params.color;
+    return (
+
+      <View style={{ flex:1, backgroundColor: color }}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+        }
       </View>
+
     )
   }
 }
+
+
+
+
+
+
+
+
+// render() {
+//     let name = this.props.route.params.name;
+//     this.props.navigation.setOptions({ title: name });
+//     let color = this.props.route.params.color;
+//     return (
+//       <View style={{flex:1, justifyContent: 'center', alignItems: 'center', backgroundColor: color}}>
+//         <Text>Hello Screen2!</Text>
+//       </View>
+//     )
+//   }
+// }
